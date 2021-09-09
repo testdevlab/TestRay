c6ab46a97ead11a146c5ee5e9260f93e0e389097
Einars Netlis-Galejs <einars.n.galejs@testdevlab.com>
2021-09-09 09:16:46 +0300

    Linux install script improvements

--- install_appium_lib.sh
+++ install_appium_lib.sh
@@ -4,12 +4,16 @@ cd /tmp
 if [[ -e ruby_lib ]]; then
 	rm -r ruby_lib
 fi
-if [[ -n $(gem list | grep rubocop) ]]; then
-	git clone https://github.com/EinarsNG/ruby_lib.git
-	cd ruby_lib
-	rake install
-	cd ..
-	rm -r ruby_lib
+if [[ -z $(which git) || -z $(which gem) || -z $(which rake) ]]; then
+	echo "git, gem and rake must be present on the system"
 else
-	echo Install Rubocop first: gem install rubocop
+	if [[ -n $(gem list | grep rubocop) ]]; then
+		git clone https://github.com/EinarsNG/ruby_lib.git
+		cd ruby_lib
+		rake install
+		cd ..
+		rm -r ruby_lib
+	else
+		echo Install Rubocop first: gem install rubocop
+	fi
 fi
