35e0484fc33de2ed229b5992037aaebf98f06ffc
Einars Netlis-Galejs <einars.n.galejs@testdevlab.com>
2021-09-09 08:23:17 +0300

    Added an install script for a custom appium_lib library

---
+++ install_appium_lib.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /tmp
+if [[ -e "ruby_lib" ]]; then
+	echo "ruby_lib folder is found in /tmp directory"
+	read -r -p "Delete the folder? (y/N): " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+		rm -r ruby_lib
+	else
+		echo "ruby_lib folder must be deleted from the /tmp directory to be able to install appium_lib"
+		exit
+	fi
+fi
+if [[ -z $(which git) || -z $(which gem) || -z $(which rake) ]]; then
+	echo "git, gem and rake must be present on the system"
+else
+	if [[ -z $(gem list | grep rubocop) ]]; then
+		read -r -p "Rubocop not found. Would you like to install it? (y/N): " response
+		if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+		then
+			gem install rubocop
+		else
+			echo "Rubocop is required for this script for build appium_lib"
+			exit
+		fi
+	fi
+	git clone https://github.com/EinarsNG/ruby_lib.git
+	cd ruby_lib
+	rake install
+	cd ..
+	rm -r ruby_lib
+fi
