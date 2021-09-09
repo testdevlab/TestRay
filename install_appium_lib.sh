703a5d24a621ac35f22eaf80f71ae865c5cccb13
Einars Netlis-Galejs <einars.n.galejs@testdevlab.com>
2021-09-09 08:30:39 +0300

    Changed the appium install script

--- install_appium_lib.sh
+++ install_appium_lib.sh
@@ -4,8 +4,12 @@ cd /tmp
 if [[ -e ruby_lib ]]; then
 	rm -r ruby_lib
 fi
-git clone https://github.com/EinarsNG/ruby_lib.git
-cd ruby_lib
-rake install
-cd ..
-rm -r ruby_lib
+if [[ -n $(gem list | grep rubocop) ]]; then
+	git clone https://github.com/EinarsNG/ruby_lib.git
+	cd ruby_lib
+	rake install
+	cd ..
+	rm -r ruby_lib
+else
+	echo Install Rubocop first: gem install rubocop
+fi
