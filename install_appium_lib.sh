6bba099384c56179d3c022070c487bcd3fed7fe4
Einars Netlis-Galejs <einars.n.galejs@testdevlab.com>
2021-09-09 10:17:54 +0300

    More improvements to the install script

--- install_appium_lib.sh
+++ install_appium_lib.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 cd /tmp
-if [[ -e ruby_lib ]]; then
-	rm -r ruby_lib
+if [[ -e "ruby_lib" ]]; then
+	echo "ruby_lib folder is found in /tmp directory"
+	read -r -p "Delete the folder? (y/N): " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	then
+		rm -r ruby_lib
+	else
+		echo "ruby_lib folder must be deleted from the /tmp directory to be able to install appium_lib"
+		exit
+	fi
 fi
 if [[ -z $(which git) || -z $(which gem) || -z $(which rake) ]]; then
 	echo "git, gem and rake must be present on the system"
