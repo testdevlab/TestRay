f7a882d58609c916cacba826f5f5fc782f9fb5d8
Einars Netlis-Galejs <einars.n.galejs@testdevlab.com>
2021-09-09 11:51:19 +0300

    ' Added Windows install batch script and changed Gemfile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source "https://rubygems.org"
 
-gem "appium_lib", git: "https://github.com/EinarsNG/ruby_lib.git"
-
 gemspec
---
+++ install_appium_lib.bat
@@ -0,0 +1,36 @@
+@echo off
+set found=no
+for /f %%i in ('dir /b') do (
+    if "%%~ni"=="ruby_lib" set found=yes
+)
+if %found%=="yes" (
+    echo ruby_lib found in the temp folder which is interfering with the install script... would you like to delete it?
+    set /p answer="Delete (y/N)?: "
+    set delete=no
+    if /i "%answer%" EQU "y" (
+        set delete=yes
+    )
+    if "%delete%"=="no" (
+        echo ruby_lib folder must be deleted in from the temp folder for the script for proceed
+    ) else (
+        rmdir /s /q "ruby_lib"
+    )
+) else (
+    git clone https://github.com/EinarsNG/ruby_lib.git
+    cd ruby_lib
+
+    set rubocop=no
+    for /f %%i in ('gem list --no-version') do (
+        if "%%i"=="rubocop" (
+            set rubocop=yes
+        )
+    )
+
+    if %rubocop%=="no" (
+        echo Rubocop is missing... installing
+        gem install rubocop
+    )
+    rake install
+    cd ..
+    rmdir /s /q "ruby_lib"
+)
