c6451e000aad0d51af4de060b1fe8cde613c16d5
Edgars Eglitis <edgars.eglitis@testdevlab.com>
2024-07-05 16:09:11 +0300

    fix: adjust timeouts for click actions

--- lib/core/device.rb
+++ lib/core/device.rb
@@ -447,17 +447,17 @@ class Device
   #   OffsetFractionY
   #   NoRaise
   def click(action)
-    start = Time.now
+    before_find = Time.now
     el = wait_for(action)
     return unless el
 
-    now = Time.now
-    log_info("Time to find element: #{now - start}s") if action["CheckTime"]
+    after_find = Time.now
+    log_info("Time to find element: #{after_find - before_find}s") if action["CheckTime"]
     error = nil
 
     wait_time = (action["CheckTime"] ? action["CheckTime"] : @timeout)
 
-    while (Time.now - start) < wait_time
+    while (Time.now - after_find) < wait_time
       begin
         @platform == "iOS" ?
           @driver.action.move_to(el) :
@@ -478,7 +478,7 @@ class Device
         else
           el.click
         end
-        log_info("Time for click: #{Time.now - start}s") if action["CheckTime"]
+        log_info("Time for click: #{Time.now - after_find}s") if action["CheckTime"]
         return
       rescue => e
         error = e
@@ -535,20 +535,20 @@ class Device
   #   CheckTime
   #   NoRaise
   def press(action)
-    start = Time.now
+    before_find = Time.now
     el = wait_for(action)
     return unless el
 
-    now = Time.now
-    log_info("Time to find element: #{now - start}s") if action["CheckTime"]
+    after_find = Time.now
+    log_info("Time to find element: #{after_find - before_find}s") if action["CheckTime"]
     error = nil
 
     wait_time = (action["CheckTime"] ? action["CheckTime"] : @timeout)
 
-    while (Time.now - start) < wait_time
+    while (Time.now - after_find) < wait_time
       begin
         @driver.action.move_to(el).pointer_down(:left).release.perform
-        log_info("Time for click: #{Time.now - start}s") if action["CheckTime"]
+        log_info("Time for click: #{Time.now - after_find}s") if action["CheckTime"]
         return
       rescue => e
         error = e
@@ -568,17 +568,17 @@ class Device
   #   CheckTime
   #   NoRaise
   def click_and_hold(action)
-    start = Time.now
+    before_find = Time.now
     el = wait_for(action)
     return unless el
 
-    now = Time.now
-    log_info("Time to find element: #{now - start}s") if action["CheckTime"]
+    after_find = Time.now
+    log_info("Time to find element: #{after_find - before_find}s") if action["CheckTime"]
     error = nil
 
     wait_time = (action["CheckTime"] ? action["CheckTime"] : @timeout)
 
-    while (Time.now - start) < wait_time
+    while (Time.now - after_find) < wait_time
       begin
         @platform == "iOS" ?
           @driver.action.move_to(el) :
@@ -588,7 +588,7 @@ class Device
       end
       begin
         @driver.action.click_and_hold(el).perform
-        log_info("Time for click and hold: #{Time.now - start}s") if action["CheckTime"]
+        log_info("Time for click and hold: #{Time.now - after_find}s") if action["CheckTime"]
         return
       rescue => e
         error = e
